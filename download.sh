67bbfee621fe4cd030a63fe88b5f504f1dd9218c
by-sabbir <me@sabbir.dev>
2023-07-15 21:07:27 +0600

    Download helpers and Gitignore

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir tasks;
+curl -o ./tasks/pose-landmark.task https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task;
