ab771f7e03664121067708b8e3f485908b22a909
by-sabbir <me@sabbir.dev>
2023-07-15 21:01:03 +0600

    Render Example video side-by-side

--- render-example.py
+++ render-example.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+import sys
 
 shape = (768, 432)
 
-ocap = cv2.VideoCapture("./data/out.mp4")
-icap = cv2.VideoCapture("./data/vid2.mp4")
+if len(sys.argv) != 4:
+    print("python download.py [output name] [original] [estimated]")
+    sys.exit(1)
+ocap = cv2.VideoCapture(sys.argv[2])
+icap = cv2.VideoCapture(sys.argv[3])
 out = cv2.VideoWriter(
-    "./data/post-estimation.mp4", cv2.VideoWriter_fourcc(*"MPEG"), 30, (864, 768)
+    f"./data/{sys.argv[1]}.mp4", cv2.VideoWriter_fourcc(*"MPEG"), 30, (864, 768)
 )
 
 while True:
@@ -31,7 +35,7 @@ while True:
     )
     h = cv2.putText(
         h,
-        "Inferred",
+        "Estimated",
         (432 + 5, 25),
         cv2.FONT_HERSHEY_COMPLEX,
         1,
